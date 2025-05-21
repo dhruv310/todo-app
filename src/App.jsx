57fae6f7202d87a39cd0e7e46501d36ff058f46f
Dhruv Patil <dhruvpatil31.2003@gmail.com>
2025-05-21 11:45:54 +0530

    Initial commit: Setup React ToDo app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+  
+
+function App() {
+   
+
+  return (
+    <>
+        <div>
+          <h2>Welcome to The ToDo App !</h2>
+        </div>
+
+        <Todo></Todo>
+    </>
+  )
+}
+import { Todo } from './component/Todo'
+
+export default App
---
+++ src/component/Todo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Todo = () => {
+  return (
+    <div>
+        <h2>Welcome From the Todo component</h2>
+    </div>
+  )
+}
